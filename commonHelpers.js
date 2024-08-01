4cdc0530ccbc60a11f99d7ae8a43ab5b5718f245
z9877969 <46637279+z9877969@users.noreply.github.com>
2024-08-01 07:48:04 +0000

    Deploying to gh-pages from @ z9877969/evoplay_test_2@4df7fe88bad4d89d80946277666cd7f560c8677f 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/burger-067ae199.js";import{S as e}from"./assets/vendor-688b6f4b.js";new e("#slider1",{perPage:4,start:1,focus:"center",autoplay:!0,rewind:!0,speed:600,interval:2e3,gap:32,trimSpace:!0,arrows:!1,pagination:!1,fixedWidth:380,fixedHeight:529,breakpoints:{1439:{perPage:3,gap:16,fixedWidth:220,fixedHeight:306,trimSpace:!1}}}).mount();
+//# sourceMappingURL=commonHelpers.js.map
